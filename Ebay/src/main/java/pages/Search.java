e1330442d2ac9dedd59ee4f6b0c226e8fa2b2937
venkat <venkatgn.gutta@gmail.com>
2022-10-19 20:16:49 +0530

    updated search.java with methods

--- Ebay/src/main/java/pages/Search.java
+++ Ebay/src/main/java/pages/Search.java
@@ -2,8 +2,13 @@ package pages;
 
 public class Search
 {
-public void searchproduct()
-{
+	public void searchproduct()
+	{
+		System.out.println("search the product");
+	}
+	public void addtoCart()
+	{
+		System.out.println("add product to cart");
 	
-}
+	}
 }
